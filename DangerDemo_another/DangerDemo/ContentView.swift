e57b6f7cf7e62d07f7f23ea446b6252471a1ec12
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2025-05-14 18:01:54 +0900

    wip

---
+++ DangerDemo_another/DangerDemo/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  DangerDemo
+//
+//  Created by Hiroki Ikeuchi on 2025/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    static let Hoge: String = "hikeuchi"
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+
+
+
+        }
+        .padding()
+        .onAppear() {
+            if true {
+                print("Nice")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DangerDemo_another/DangerDemo/DangerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerDemoApp.swift
+//  DangerDemo
+//
+//  Created by Hiroki Ikeuchi on 2025/05/12.
+//
+
+import SwiftUI
+
+@main
+struct DangerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
