a13318ac0af945960b124017cfb9f9d9f21c4fff
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2025-05-14 17:30:00 +0900

    wip

--- DangerDemo/DangerDemo/ContentView.swift
+++ DangerDemo/DangerDemo/ContentView.swift
@@ -8,21 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    static let Hoge: String = "hikeuchi"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+
+
+
         }
         .padding()
-        
-     
-        
-        
-        
-        
-        
+        .onAppear() {
+            if true {
+                print("Nice")
+            }
+        }
     }
 }
 
