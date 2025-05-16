9ac913958802f79e0674d460307267eb2ef92796
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2025-05-16 17:33:45 +0900

    wip

--- Danger/Dangerfile.swift
+++ Danger/Dangerfile.swift
@@ -5,17 +5,32 @@ import Foundation
 
 let backlogKeyPrefix = "FONTELIER_V2"
 
-// MARK: - Property
+// MARK: - Helpers
 
-let danger = Danger()
-var isAllCheckPassed = false // 全てのチェックに成功したかどうか
+// MARK: ブランチ名の確認
+
+func checkBranchName(headBranchName: String) {
+    // head(例: feature) -> base(例: develop)
+    let expectedPrefixes = ["release", "master", "main", "develop", "develop_ent", "feature-", "revert", "hotfix"];
+
+    // ブランチ名が想定する名称かどうかの確認
+    guard let _ = expectedPrefixes.first(where: { headBranchName.hasPrefix($0) }) else {
+        warn("""
+ブランチ名が不正です
+次のいずれかで始まるブランチ名にしてください: \(expectedPrefixes.joined(separator: ", "))
+""")
+        return
+    }
+}
+
+// MARK: PRの課題タイトルにBacklogの課題番号が記載されていることの確認
 
 /// 指定されたパターンで文字列を抽出する関数
 /// - Parameters:
 ///   - input: 検索対象の文字列
 ///   - pattern: 抽出する正規表現パターン
 /// - Returns: 抽出した文字列またはnil
-private func extractWithRegex(input: String, pattern: String) -> String? {
+func extractWithRegex(input: String, pattern: String) -> String? {
     do {
         let regex = try NSRegularExpression(pattern: pattern)
         let matches = regex.matches(in: input, range: NSRange(input.startIndex..., in: input))
@@ -36,18 +51,19 @@ func extractBacklogKeyFromTitle(_ title: String) -> String? {
     return extractWithRegex(input: title, pattern: "\(backlogKeyPrefix)-\\d+")
 }
 
-// MARK: PRの課題タイトルにBacklogの課題番号が記載されていることの確認
-
-let backlogKey = extractBacklogKeyFromTitle(danger.github.pullRequest.title)
-if let backlogKey {
-    message("[Backlogの対応課題](https://morisawa.backlog.jp/view/\(backlogKey))")
-} else {
-    warn(#"タイトルにBacklogの課題番号"\#(backlogKeyPrefix)"が含まれていません。"#)
+func checkPRTitle(_ title: String) {
+    let backlogKey = extractBacklogKeyFromTitle(title)
+    if let backlogKey {
+        message("[Backlogの対応課題 FONTELIER_V2-\(backlogKey)](https://morisawa.backlog.jp/view/\(backlogKey))")
+    } else {
+        warn(#"タイトルにBacklogの課題番号"\#(backlogKeyPrefix)"が含まれていません。"#)
+    }
 }
 
-// MARK: SwiftLintの実行
-//SwiftLint.lint()
-//
-//SwiftLint.lint(.modifiedAndCreatedFiles(directory: "DangerDemo/"),
-//               inline: true,
-//               configFile: "DangerDemo/swiftlint.yml")
+// MARK: - Entry Point
+
+let danger = Danger()
+var isAllCheckPassed = false // 全てのチェックに成功したかどうか
+
+checkBranchName(headBranchName: danger.github.pullRequest.head.ref)
+checkPRTitle(danger.github.pullRequest.title)
